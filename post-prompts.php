7e5aa5c5f7e13b6657e0d5f492887e517e1ac2dd
ryelle <kelly.dwan@gmail.com>
2014-11-01 23:36:17 -0400

    Revamp plugin to use RSS for dailypost.wordpress.com, since we can’t access the dp_prompt post type via API. Switch to a Dashboard widget instead of button on the welcome panel.

--- post-prompts.php
+++ post-prompts.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
  * Plugin Name: Post Prompts
  * Author: Kelly Dwan
  * Author URI: http://redradar.net
- * Version: 1.0
+ * Version: 0.1.0
  * Description: Set up posts with prompts for daily writing.
  */
 
@@ -13,6 +13,11 @@ class KD_PostPrompts {
 	 * Singleton
 	 */
 	private static $instance;
+
+	/**
+	 * Store our prompt
+	 */
+	private static $prompt;
 
 	/**
 	 * Silence is golden!
@@ -39,61 +44,130 @@ class KD_PostPrompts {
 	 * @return  void
 	 */
 	public function setup() {
-		add_action( 'welcome_panel', array( $this, 'welcome_panel' ) );
-		add_filter( 'default_content', array( $this, 'default_content' ), 10, 2 );
+		add_filter( 'default_content', array( $this, 'default_content' ), 10 );
+		add_filter( 'default_title', array( $this, 'default_title' ), 10 );
+		add_action( 'wp_dashboard_setup', array( $this, 'add_to_dashboard' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'styles' ) );
 	}
 
 	/**
-	 * Add buttons to the welcome panel.
+	 * Add plugin-specific styles.
+	 *
+	 * @return  void
 	 */
-	function welcome_panel() { 
-		$url = admin_url( 'post-new.php' );
-		$examen = add_query_arg( array( 'prompt' => 'examen' ), $url );
-		$daily = add_query_arg( array( 'prompt' => 'daily-prompt' ), $url );
-		?>
-		<div style="margin: 0 -10px; padding: 10px 20px 20px; border-top: 1px solid #eee;">
-		<h3 style="margin-bottom: 10px;">Prompts</h3>
-		<a class="button" href="<?php echo $examen; ?>">Daily Reflection</a> &nbsp;
-		<a class="button" href="<?php echo $daily; ?>">WP.com Daily Prompt</a>
-		</div>
-	<?php }
+	public function styles(){
+		wp_enqueue_style( 'dailyprompt', plugins_url( 'style.css', __FILE__ ) );
+	}
 
 	/**
-	 * Replace the default post content with the selected prompt
+	 * Add the dashboard widget.
+	 *
+	 * @return  void
 	 */
-	function default_content( $post_content, $post ) {
-		if ( 'examen' == $_GET['prompt'] ){
+	public function add_to_dashboard(){
+		add_meta_box( 'kd-daily-prompt', __( 'Prompt', 'kd_prompts' ), array( $this, 'dashboard_widget' ), 'dashboard', 'side', 'high' );
+	}
 
-			$post_content = "<h1>What was today’s low point? What was the worst part of your day? What was your biggest struggle today, or when did you feel sad, helpless or angry?</h1><h1>What was today’s high point? What was the best part of your day? What did you feel good about today?</h1>";
+	/**
+	 * Display the prompt and button on the Dashboard widget.
+	 *
+	 * @return  void
+	 */
+	public function dashboard_widget() {
+		$url = admin_url( 'post-new.php' );
+		$post = self::get_daily_prompt();
 
-		} elseif ( 'daily-prompt' == $_GET['prompt'] ) {
+		echo '<p>'. sprintf( esc_html__( "Need a blog topic? Here's the latest daily prompt from %s.", 'kd_prompts' ), '<a href="http://href.li/?http://dailypost.wordpress.com/">WordPress.com Daily Post</a>' ) .'</p>';
 
-			$url = 'https://public-api.wordpress.com/rest/v1/sites/dailypost.wordpress.com/posts/';
-			$url = add_query_arg( array(
-				'category' => 'daily-prompts',
-				'number' => 1,
-			), $url );
-
-			$response = wp_remote_get( $url );
-
-			if ( ! is_wp_error( $response ) ) {
-
-				$body = json_decode( wp_remote_retrieve_body( $response ) );
-
-				if ( is_object( $body ) && ! empty( $body->posts ) ) {
-
-					$post_content = sprintf( '<h1><a href="%s">%s</a></h1>%s',
-						$body->posts[0]->URL,
-						$body->posts[0]->title,
-						$body->posts[0]->content
-					);
-
-				}
-
-			}
-
+		if ( is_array( $post ) ){
+			printf( '<blockquote>%1$s<cite><a href="%2$s">%3$s</a></cite></blockquote>',
+				wp_kses_post( $post['content'] ),
+				esc_url( 'http://href.li/?' . $post['URL'] ),
+				esc_html( $post['title'] )
+			);
+		} else {
+			// Could be better.
+			esc_html_e( 'There was an error getting the prompt', 'kd_prompts' );
 		}
+
+		printf( '<p><a class="button" href="%s">%s</a></p>',
+			esc_url( add_query_arg( array( 'prompt' => 'daily-prompt' ), $url ) ),
+			esc_html__( 'Start a post with this prompt', 'kd_prompts' )
+		);
+	}
+
+	/**
+	 * Replace the default post content with the selected prompt, if the URL parameter is set.
+	 *
+	 * @param   string   $post_content  The default post content
+	 * @return  string  The default post content, with today's prompt if available.
+	 */
+	public function default_content( $post_content ) {
+		if ( ! isset( $_GET['prompt'] ) )
+			return $post_content;
+
+		if ( 'daily-prompt' == $_GET['prompt'] ) {
+
+			$prompt = self::get_daily_prompt();
+
+			if ( is_array( $prompt ) ){
+				$post_content = sprintf( '<blockquote>%1$s<cite><a href="%2$s">%3$s</a></cite></blockquote> &nbsp;',
+					wp_kses_post( $prompt['content'] ),
+					esc_url( $prompt['URL'] ),
+					esc_html( $prompt['title'] )
+				);
+			}
+		}
+
 		return $post_content;
 	}
+
+	/**
+	 * Replace the default post title with the selected prompt, if the URL parameter is set.
+	 *
+	 * @param   string   $post_title  The default post title
+	 * @return  string  The default post title, with today's prompt name if available.
+	 */
+	public function default_title( $post_title ) {
+		if ( isset( $_GET['prompt'] ) && ( 'daily-prompt' == $_GET['prompt'] ) ) {
+			$prompt = self::get_daily_prompt();
+			$post_title = sprintf( __( 'Prompt: %s', 'kd_prompts' ), $prompt['title'] );
+		}
+
+		return $post_title;
+	}
+
+	/**
+	 * Get the Daily Prompt from WordPress.com (dailypost.wordpress.com)
+	 *
+	 * @return  Object|bool  Post-like object (http://developer.wordpress.com/docs/api/1/get/sites/%24site/posts/),
+	 *                       or false if no post returned.
+	 */
+	public function get_daily_prompt() {
+		// Check if we've already pulled down the prompt, and return it if so. Bypasses 2nd feed read.
+		if ( isset( self::$prompt ) && is_array( self::$prompt ) ) {
+			return self::$prompt;
+		}
+
+		$url = 'http://dailypost.wordpress.com/dp_prompt/feed/';
+		$rss = fetch_feed( $url );
+
+		if ( ! is_wp_error( $rss ) ) {
+
+			$item = $rss->get_item();
+
+			$prompt = array();
+			$prompt['URL']     = $item->get_permalink();
+			$prompt['title']   = $item->get_title();
+			$prompt['content'] = wpautop( strip_tags( $item->get_content() ) );
+
+			self::$prompt = $prompt;
+			return $prompt;
+		}
+
+		return false;
+	}
+
 }
+
 KD_PostPrompts::instance();
